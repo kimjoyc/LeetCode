7253b9c199709fbcdf23911e0db89654535e0d7b
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-10-02 13:07:00 -0700

    Update MaximumSubarray.cpp

--- Sequences/MaximumSubarray.cpp
+++ Sequences/MaximumSubarray.cpp
@@ -34,6 +34,32 @@ Constraints:
 
 Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
 
+        // Initialize variables to store the current and maximum subarray sums
+        int currSubArr_sum = nums[0];
+        int maxSubArr_sum = nums[0];
+
+        // Iterate through the array starting from the second element
+        for (int i = 1; i < nums.size(); i++) {
+
+            // Calculate the current subarray sum by choosing the maximum between the current element and the sum of the previous subarray plus the current element
+            currSubArr_sum = max(nums[i], nums[i] + currSubArr_sum);
+
+            // Update the maximum subarray sum by choosing the maximum between the previous maximum subarray sum and the current subarray sum
+            maxSubArr_sum = max(maxSubArr_sum, currSubArr_sum);
+        }
+
+        // Return the maximum subarray sum
+        return maxSubArr_sum;
+    }
+};
 /*
+Time Complexity:
+The time complexity of this function is O(n), where n is the size of the input vector nums. This is because the function iterates through the input vector once, performing a constant number of operations for each element.
+
+Space Complexity:
+The space complexity is O(1), indicating constant space usage. The function uses only a few integer variables (currSubArr_sum and maxSubArr_sum) to keep track of the current and maximum subarray sums. The space used is independent of the input size and remains constant.
 */
