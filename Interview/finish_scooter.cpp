d7902e9afbdd052fbe5a08527833d6b05a6afd7f
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-10-16 11:52:23 -0700

    Create finish_scooter.cpp

---
+++ Interview/finish_scooter.cpp
@@ -0,0 +1,6 @@
+/*
+Imagine that you are standing at the starting point of a straight street and are trying to reach the end of the street. The street is represented by a
+number line starting at 0 and ending at finish (finish > 0).
+
+There are electric scooters scattered along to help you get to the end 
+*/
