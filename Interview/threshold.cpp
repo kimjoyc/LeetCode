54960e52708d2c95223dfb5e0af71e754e1f50b0
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-10-16 11:09:07 -0700

    Update threshold.cpp

--- Interview/threshold.cpp
+++ Interview/threshold.cpp
@@ -15,6 +15,15 @@ When i =2, the triples (numbers[2], numbers[3], numbers[4]) = (4, 3, 2) contains
 
 Thus, answer is i = 2.
 
+For numbers = [-9, 95, 94, 4, 51] and threshold = 42 the output should be solution(numbers, threshold) = -1
+
+Explanation:
+When i = 0 the triple (numbers[0], numbers[1], numbers[2]) = (-9, 95, 94) contains -9 < 42 which does not satisfy condition. 
+
+When i = 1 the triple (numbers[1], numbers[2], numbers[3]) = (95, 94, 4) contains 4 < 42 which does not satisfy the condition 
+
+When i = 2 the triple (numbers[2], numbers[3], numberes[4]) = (94, 4, 51) contains 4 < 42 does not satisfy the condition return -1.
+
 
 
 */
