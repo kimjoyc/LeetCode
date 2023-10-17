1b248827aa45c5f7eafd968537e31d22501bd7b5
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-10-17 11:40:19 -0700

    Update lamp.cpp

--- Interview/lamp.cpp
+++ Interview/lamp.cpp
@@ -1,3 +1,17 @@
 /*
+There are some lamps placed on coordinate line. Each of these lamps illuminates some space around it within a given radius.
 
+You are given the coordinates of the lamps on the line, and the effective radius of each lamps' light.
+
+In other words, you are given a two-dimensional array lamps, where lamps[i] contains information about i-th lamp.
+
+
+lamps[i][0] is an integer representing the lamp's coordinate
+lamps[i][1] is a positive integer representing the lamps' effective radius.
+That means that the i-th lamp illuminates everything from the range from lamps[i][0] - lamps[i][1] to lamps[i][0] + lamps[i][1] inclusive.
+
+Your task is to find the number of integer coordinates that are illuminated exactly by 1 lamp.
+
+
+Example: 
 */
