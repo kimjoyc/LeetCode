d6205922b0dab59838e807c894f2a49452db548e
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-10-18 15:45:30 -0700

    Update groupanagram.cpp

--- Sequences/groupanagram.cpp
+++ Sequences/groupanagram.cpp
@@ -38,6 +38,39 @@ strs[i] consists of lowercase English letters.
 */
 
 
+To group anagrams together, we can use a hash map where the key is a sorted version of each string (representing its characters in alphabetical order), and the value is a list of strings that match that sorted version. Here's a C++ solution to achieve this:
+
+cpp
+Copy code
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Create a hash map to store groups of anagrams
+        std::unordered_map<std::string, std::vector<std::string>> anagramGroups;
+        
+        for (const std::string& str : strs) {
+            // Sort the characters of the string to use as a key
+            std::string sortedStr = str;
+            std::sort(sortedStr.begin(), sortedStr.end());
+            
+            // Add the original string to the corresponding group
+            anagramGroups[sortedStr].push_back(str);
+        }
+        
+        // Transform the hash map into the required output format
+        std::vector<std::vector<std::string>> result;
+        for (const auto& pair : anagramGroups) {
+            result.push_back(pair.second);
+        }
+        
+        return result;
+    }
+};
 /*
 
 */
