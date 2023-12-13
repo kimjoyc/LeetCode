55e0e2b7e6b27c2e823276e83429748c4c7bca30
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-12-13 14:52:05 -0800

    Update InvertFlipBinaryTree.cpp

--- Non-lineardatastructures/InvertFlipBinaryTree.cpp
+++ Non-lineardatastructures/InvertFlipBinaryTree.cpp
@@ -28,3 +28,39 @@ Constraints:
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        // Base case: if the root is null, return null.
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        // Swap the left and right subtrees of the current node.
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        // Recursively invert the left and right subtrees.
+        invertTree(root->left);
+        invertTree(root->right);
+
+        // Return the root of the inverted tree.
+        return root;
+
+
+        
+    }
+};
