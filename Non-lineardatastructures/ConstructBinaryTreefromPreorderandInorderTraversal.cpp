400e56218206808c7f8926a1984ad7a39c7c688e
Joyce Kim <88675769+kimjoyc@users.noreply.github.com>
2023-12-22 16:54:13 -0800

    Update ConstructBinaryTreefromPreorderandInorderTraversal.cpp

--- Non-lineardatastructures/ConstructBinaryTreefromPreorderandInorderTraversal.cpp
+++ Non-lineardatastructures/ConstructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -28,3 +28,55 @@ Each value of inorder also appears in preorder.
 preorder is guaranteed to be the preorder traversal of the tree.
 inorder is guaranteed to be the inorder traversal of the tree
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return buildTreeHelper(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+    }
+
+private:
+    TreeNode* buildTreeHelper(vector<int>& preorder, int preStart, int preEnd,
+                              vector<int>& inorder, int inStart, int inEnd) 
+    {
+        // Base case: empty subtree
+        if (preStart > preEnd || inStart > inEnd) 
+        {
+            return nullptr;
+        }
+
+        // The first element in preorder is the root of the current subtree
+        int rootValue = preorder[preStart];
+        TreeNode* root = new TreeNode(rootValue);
+
+        // Find the index of the root value in inorder
+        int rootIndex = inStart;
+        while (inorder[rootIndex] != rootValue) 
+        {
+            rootIndex++;
+        }
+
+        // Calculate the size of left subtree
+        int leftSubtreeSize = rootIndex - inStart;
+
+        // Recursively build left and right subtrees
+        root->left = buildTreeHelper(preorder, preStart + 1, preStart + leftSubtreeSize,
+                                    inorder, inStart, rootIndex - 1);
+        root->right = buildTreeHelper(preorder, preStart + leftSubtreeSize + 1, preEnd,
+                                    inorder, rootIndex + 1, inEnd);
+
+        return root;
+    }
+
+    
+};
